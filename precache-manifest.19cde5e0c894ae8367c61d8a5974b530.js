e9f77d9cce8cad09b1c822bf50b5f373e8b9b3c0
prem S N <prem.sn.eee@gmail.com>
2020-10-31 11:34:55 +0530

    Updates

---
+++ precache-manifest.19cde5e0c894ae8367c61d8a5974b530.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a69eea45c4eea98447ec564fc151c821",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "700103e040a004d0de44",
+    "url": "/recipe-app/static/css/main.bdcff1f6.chunk.css"
+  },
+  {
+    "revision": "fcc5847fb12328b5b37a",
+    "url": "/recipe-app/static/js/2.d04fa6f2.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/recipe-app/static/js/2.d04fa6f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "700103e040a004d0de44",
+    "url": "/recipe-app/static/js/main.28fb8615.chunk.js"
+  },
+  {
+    "revision": "95f8ec566e7199ca77cb",
+    "url": "/recipe-app/static/js/runtime-main.5e04f524.js"
+  }
+]);
